875095f08980b1bb04db79948b89a2a94d234c80
Ahmedsaleh1234 <a12661826@gmail.com>
2025-06-17 01:41:29 +0300

    updating url on server

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('archievesystem.urls')),
@@ -26,5 +27,10 @@ urlpatterns = [
 
     path('ai/', include('ocr_app.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Add route to serve media files through Django in production
+    urlpatterns += [
+        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
