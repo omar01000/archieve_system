5afebc25abc907941078621a30d345c2d36ddfd4
Ahmedsaleh1234 <a12661826@gmail.com>
2025-06-17 02:12:29 +0300

    updating url on server

--- project/urls.py
+++ project/urls.py
@@ -15,22 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve 
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('archievesystem.urls')),
+    path('', include('archievesystem.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls')),
-
     path('ai/', include('ocr_app.urls')),
 ]
+
+# Serve media files in both development and production
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    # Add route to serve media files through Django in production
+    # Serve media files in production
     urlpatterns += [
-        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
