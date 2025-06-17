ba1aa1a6131a54e885f05f4b7d66958f1a0e1e07
omar01000 <aomar9452@gmail.com>
2025-06-17 09:33:41 +0300

    cloudinary

--- project/settings.py
+++ project/settings.py
@@ -139,18 +139,19 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Media files - Use Cloudinary in production, local storage in development
-# if os.environ.get("CLOUDINARY_CLOUD_NAME"):
-#     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-#     CLOUDINARY_STORAGE = {
-#         'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-#         'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-#         'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-#     }
-# else:
-    # Local development media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# ✅ Cloudinary إعدادات
+if os.environ.get("CLOUDINARY_CLOUD_NAME"):
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    }
+else:
+    # 🟡 fallback في حالة عدم توفر بيانات كلاودنيري (local dev)
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
